3c0c38f9f4aea310c5daa2048931460c8973e481
rahman318 <rahman_uwl@edenzil.com>
2025-11-27 14:47:44 +0800

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,28 +1,28 @@
 import * as Brevo from '@getbrevo/brevo';
 
 const client = new Brevo.TransactionalEmailsApi();
-client.setApiKey(process.env.BREVO_API_KEY);
 
+// Fungsi hantar email
 const sendEmail = async ({ to, subject, html, attachments }) => {
   try {
-    let brevoAttachments = [];
-    if (attachments?.length) {
-      brevoAttachments = attachments.map(file => ({
-        content: file.content.toString('base64'),
-        name: file.filename,
-        type: file.mimetype || 'application/octet-stream',
-      }));
-    }
+    // Map attachments
+    const brevoAttachments = attachments?.map(file => ({
+      content: file.content.toString('base64'),
+      name: file.filename,
+      type: file.mimetype || 'application/octet-stream',
+    }));
 
+    // Request body
     const emailData = {
       sender: { name: 'e-Approval System', email: 'admin@underwaterworldlangkawi.com' },
       to: [{ email: to }],
       subject,
       htmlContent: html,
-      attachment: brevoAttachments.length ? brevoAttachments : undefined,
+      attachment: brevoAttachments?.length ? brevoAttachments : undefined,
     };
 
-    await client.sendTransacEmail(emailData);
+    // API call → pass API key sebagai header
+    await client.sendTransacEmail(emailData, { 'api-key': process.env.BREVO_API_KEY });
 
     console.log(`✅ Emel berjaya dihantar kepada: ${to}`);
   } catch (error) {
