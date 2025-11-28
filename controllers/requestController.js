b934a0e758e95dfdd0cbe0b2f4adc17ecce5c9d6
rahman318 <rahman_uwl@edenzil.com>
2025-11-28 16:38:04 +0800

    Update requestController.js

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -302,7 +302,8 @@ export const updateRequestStatus = async (req, res) => {
 
     res.status(200).json(request);
       
-   } catch (err) {
+      { catch (err) 
+  {
     console.error("❌ Error updateRequestStatus:", err.message);
     res.status(500).json({ message: "Gagal update status request" }
 
@@ -310,6 +311,7 @@ export const updateRequestStatus = async (req, res) => {
   );
 
   
+
 
 
 
