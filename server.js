8872a645e620a024eaacd2dbc5a684f4aba1b772
rahman318 <rahman_uwl@edenzil.com>
2025-12-02 11:04:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,12 +59,12 @@ app.use(express.static(path.join(__dirname, "dist"))); // <-- letak sini
 
 // Reset password fallback
 app.get("/reset-password/:token", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 // Universal SPA fallback (semua route React lain)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 // ==========================
@@ -90,6 +90,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+
 
 
 
