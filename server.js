df465b3144c09bfa6dc18f4dc6dc73e03576b892
rahman318 <rahman_uwl@edenzil.com>
2025-12-08 11:32:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ app.use("/api", testEmailRoutes);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Reset password SPA route (React)
-app.get("/reset-password/:token?", (req, res) => res.sendFile(path.join(__dirname, "dist/index.html")));
+app.get("/reset-password/:token?", (req, res) => res.sendFile(path.join(__dirname, "dist")));
 
 // Universal SPA fallback (React)
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "dist/index.html")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "dist")));
 
 // ==========================
 // ⚙️ MongoDB Connection
@@ -94,5 +94,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running at http://localhost:${PORT}`)
 );
+
 
 
