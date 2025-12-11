f3fffb556e626fbb5fa6046f08185265693b9d13
rahman318 <rahman_uwl@edenzil.com>
2025-12-11 10:14:38 +0800

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,6 +1,6 @@
 // emailService.js
 import Brevo from "@getbrevo/brevo";
-import { generateRequestPDF } from "../utils/generatePDF.js"; // pastikan path betul
+import { generateRequestPDF } from "./generatePDF.js"; // pastikan path betul
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -49,4 +49,5 @@ const sendRequestEmail = async (request) => {
 
 // 🟢 Export default supaya import boss di controller jadi smooth
 export default sendRequestEmail;
+
 
