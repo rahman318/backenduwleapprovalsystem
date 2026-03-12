511df84b039e522a30faba3e726b6ca9b89fde0d
rahman318 <rahman_uwl@edenzil.com>
2026-03-12 16:19:24 +0800

    Update generatePDFFromDB.js

--- utils/generatePDFFromDB.js
+++ utils/generatePDFFromDB.js
@@ -3,22 +3,6 @@ import { PDFDocument, StandardFonts, rgb, degrees } from "pdf-lib";
 import Request from "../models/Requests.js";
 import fetch from "node-fetch"; // pastikan install node-fetch
 
-/* ================ Waktu Malaysia =========== */
-function formatDateTime(date) {
-  if (!date) return "-";
-  const d = new Date(date);
-  // adjust +8 jam untuk Malaysia
-  d.setHours(d.getHours() + 8);
-  return d.toLocaleString("ms-MY", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-}
-
 /* ================= HELPERS ================= */
 function drawLineBelowText(page, y, startX = 50, endX = 545, offset = 5, thickness = 0.8) {
   page.drawLine({ start: { x: startX, y: y - offset }, end: { x: endX, y: y - offset }, thickness, color: rgb(0.6, 0.6, 0.6) });
@@ -35,7 +19,17 @@ async function embedPngFromBase64(pdf, base64) {
 
 function formatDateTime(date) {
   if (!date) return "-";
-  return new Date(date).toLocaleString("ms-MY", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", hour12: false });
+  const d = new Date(date);
+  // adjust +8 jam untuk Malaysia
+  d.setHours(d.getHours() + 8);
+  return d.toLocaleString("ms-MY", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
 }
 
 function getActionDate(a) {
@@ -177,4 +171,5 @@ export async function generatePDFWithLogo(requestId) {
 
   return await pdf.save({ useObjectStreams: false });
 }
+
 
