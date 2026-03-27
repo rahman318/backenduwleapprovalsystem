746b07f37bae1bfcaa6db28a521ec15f86ef9abb
rahman318 <rahman_uwl@edenzil.com>
2026-03-27 12:20:31 +0800

    Update requestController.js

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -3,7 +3,7 @@ import Request from "../models/Requests.js";
 import User from "../models/user.js";
 import { sendEmail } from "../utils/emailService.js";
 import { uploadFileToSupabase } from "../utils/supabaseUpload.js";
-import { generateGenericPDF } from "../utils/generatePDFFromDB.js";
+import { generatePDFWithLogo } from "../utils/generatePDFFromDB.js";
 import generatePDF from "../utils/generatePDF.js";
 import multer from "multer";
 
@@ -128,7 +128,7 @@ export const createRequest = async (req, res) => {
     // -------- GENERATE PDF BUFFER --------
     let pdfBuffer = null;
     try {
-      pdfBuffer = await generateGenericPDF(populatedRequest);
+      pdfBuffer = await generatePDFWithLogo(populatedRequest);
       if (!Buffer.isBuffer(pdfBuffer)) pdfBuffer = null;
     } catch (pdfErr) {
       console.error("❌ PDF generate error:", pdfErr.message);
@@ -225,7 +225,7 @@ export const approveLevel = async (req, res) => {
 
     if (allApproved) {
       try {
-        const pdfBuffer = await generateGenericPDF(request);
+        const pdfBuffer = await generatePDFWithLogo(request);
         const staffEmail = request.userId?.email;
         if (staffEmail) {
           await sendEmail({
@@ -422,7 +422,7 @@ export const downloadGenericPDF = async (req, res) => {
     const { id } = req.params;
     const request = await Request.findById(id).populate("approvals userId");
     if (!request) return res.status(404).json({ message: "Request tak jumpa" });
-    const pdfBytes = await generateGenericPDF(request);
+    const pdfBytes = await generatePDFWithLogo(request);
     res.set({ "Content-Type": "application/pdf", "Content-Disposition": `attachment; filename=Request_${id}.pdf` });
     res.send(pdfBytes);
   } catch (err) {
