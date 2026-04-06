7d70796ca4b2d208db2e95c42dfbda513983ce5a
rahman318 <rahman_uwl@edenzil.com>
2026-04-06 10:39:21 +0800

    Update Requests.js

--- models/Requests.js
+++ models/Requests.js
@@ -90,7 +90,7 @@ timeToComplete: { type: Number, default: null },
 
 finalStatus: {
   type: String,
-  enum: ["Pending", "Approved", "Rejected", "recalled"],
+  enum: ["Pending", "Approved", "Rejected", "Recalled"],
   default: "Pending",
 },
 
