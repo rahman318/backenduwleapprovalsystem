8dbc50bc0bf5942cc85dcbd8ad48db492a191596
rahman318 <rahman_uwl@edenzil.com>
2026-04-22 15:51:54 +0800

    Add files via upload

---
+++ controllers/auditLogController.js
@@ -0,0 +1,14 @@
+import AuditLog from "../models/AuditLog.js";
+
+export const getAuditLogs = async (req, res) => {
+  try {
+    const logs = await AuditLog.find()
+      .populate("userId", "name email") // ambil nama user
+      .sort({ createdAt: -1 }); // newest first
+
+    res.status(200).json(logs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to fetch audit logs" });
+  }
+};
